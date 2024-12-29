25229b73d172c54e90371e3966e3c1a6b495b7c0
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2024-12-29 19:16:59 +0900

    build: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              wabt
+            ];
+          };
+      }
+    );
+}
