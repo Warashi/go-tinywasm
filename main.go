69b692e3a8feafb45931e77ea9493c6e045b7cb0
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-01-02 21:45:17 +0900

    feat: enhance main function with structured logging and signal handling
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	_ "embed"
+	"log/slog"
+	"os/signal"
 
 	"flag"
-	"log"
 	"os"
 	"runtime/pprof"
 
@@ -13,6 +15,10 @@ import (
 )
 
 func main() {
+	os.Exit(_main())
+}
+
+func _main() (exitCode int) {
 	var prof bool
 	flag.BoolVar(&prof, "prof", false, "record cpuprofile with profile.out")
 	flag.Parse()
@@ -20,33 +26,54 @@ func main() {
 	if prof {
 		f, err := os.Create("profile.out")
 		if err != nil {
-			log.Fatalln(err)
+			slog.Error("failed to create profile.out", slog.Any("error", err))
+			return 1
 		}
 		defer func() {
 			if err := f.Close(); err != nil {
-				log.Fatalln(err)
+				slog.Error("failed to close profile.out", slog.Any("error", err))
+				exitCode = 1
 			}
 		}()
 		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatalln(err)
+			slog.Error("failed to start cpuprofile", slog.Any("error", err))
+			return 1
 		}
 		defer pprof.StopCPUProfile()
 	}
 
 	f, err := os.Open(flag.Arg(0))
 	if err != nil {
-		log.Fatalln(err)
+		slog.Error("failed to open file", slog.Any("error", err))
+		return 1
 	}
 	defer f.Close()
 
 	r, err := runtime.New(f)
 	if err != nil {
-		log.Fatalln(err)
+		slog.Error("failed to create runtime", slog.Any("error", err))
+		return 1
 	}
 
 	wasip1.NewWasiPreview1().Register(r)
 
-	if _, err := r.Call("_start"); err != nil {
-		log.Fatalln(err)
+	sigCh, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	done := make(chan int)
+	go func() {
+		defer close(done)
+		if _, err := r.Call("_start"); err != nil {
+			slog.Error("failed to call _start", slog.Any("error", err))
+			done <- 1
+		}
+	}()
+
+	select {
+	case <-sigCh.Done():
+		slog.Info("interrupted")
+		return 1
+	case c := <-done:
+		return c
 	}
 }
