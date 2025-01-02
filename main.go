12ed9f40c6592389822050e4c0b8c246c583fbc8
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-01-02 14:35:42 +0900

    style: format code for improved readability in main.go
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- main.go
+++ main.go
@@ -6,33 +6,32 @@ import (
 	"flag"
 	"log"
 	"os"
-  "runtime/pprof"
+	"runtime/pprof"
 
 	"github.com/Warashi/wasmium/runtime"
 	"github.com/Warashi/wasmium/wasip1"
 )
 
-
 func main() {
-  var prof bool
-  flag.BoolVar(&prof, "prof", false,"record cpuprofile with profile.out")
+	var prof bool
+	flag.BoolVar(&prof, "prof", false, "record cpuprofile with profile.out")
 	flag.Parse()
 
-  if prof {
-    f, err := os.Create("profile.out")
-    if err != nil {
-      log.Fatalln(err)
-    }
-    defer func() {
-      if err := f.Close(); err != nil {
-        log.Fatalln(err)
-      }
-    }()
-    if err := pprof.StartCPUProfile(f); err != nil {
-      log.Fatalln(err)
-    }
-    defer pprof.StopCPUProfile()
-  }
+	if prof {
+		f, err := os.Create("profile.out")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer func() {
+			if err := f.Close(); err != nil {
+				log.Fatalln(err)
+			}
+		}()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatalln(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 
 	f, err := os.Open(flag.Arg(0))
 	if err != nil {
